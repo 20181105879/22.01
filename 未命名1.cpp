9d6cdacc6f515ca00c480c7ee0ed5fff3a036570
20181105879 <2605753145@qq.com>
2019-12-22 19:25:51 +0800

    hjg
    ghj

--- 未命名1.cpp
+++ 未命名1.cpp
@@ -1,76 +1,69 @@
-#include <bits/stdc++.h>
-#include <cstdio>
-#include <cstring>
 #include <iostream>
+#include <string.h>
 #include <vector>
+#include <stack>
 #include <algorithm>
-#include <queue>
-#include <iomanip>
-#define MAX 1000000000
-#define inf 0x3f3f3f3f
-#define FRE() freopen("in.txt","r",stdin)
-
 using namespace std;
-typedef long long ll;
-const int maxn = 10005;
-struct Node
-{
-    char date;
-    Node* rt;
-    Node* lt;
-};
-typedef Node* Tree;
-int len,idx;
-string str;
-
-void build(Tree& root)
-{
-    if(str[idx]=='#'||idx==len) return;
-    root = new Node;
-    root->date = str[idx];
-    root->lt = NULL;
-    root->rt = NULL;
-    idx++;//因为在字符串中是一直向后走的，所以这里用一个全局变量表示字符串你的下标就ok了
-    build(root->lt);
-    idx++;
-    build(root->rt);
-    return;
-}
-
-void exchangeNode(Tree& root)//从根节点一次交换左右子树就搞定！
-{
-    if(root->lt==NULL && root->rt==NULL)
-        return;
-    Node* temp;
-    temp = root->lt;
-    root->lt = root->rt;
-    root->rt = temp;
-    if(root->lt!=NULL)
-        exchangeNode(root->lt);
-    if(root->rt!=NULL)
-        exchangeNode(root->rt);
-}
-
-void midTravel(Tree root)//中序遍历二叉树
-{
-    if(root->lt!=NULL)
-        midTravel(root->lt);
-    printf("%c",root->date);
-    if(root->rt!=NULL)
-        midTravel(root->rt);
-}
-
 int main()
 {
-   // FRE();
-    cin>>str;
-    len = str.length();
-    idx = 0;
-    Tree root = NULL;
-    build(root);
-    midTravel(root);
-    printf("\n");
-    exchangeNode(root);
-    midTravel(root);
-    return 0;
-}
+	vector<int> v;
+	stack<int> s;
+	
+	int n,m;
+	cin>>n;
+	string name;
+	int j;
+	for(int i=0;i<n;i++)
+	{
+		cin>>name;
+		if(name=="Pop")
+		{
+			if(s.empty())
+			{
+				cout<<"Invalid"<<endl;
+			}
+			else
+			{
+				cout<<s.top()<<endl;
+				//vector<int>::iterator j=lower_bound(v.begin(),v.end(),s.top());
+				//v.erase(j)
+				v.erase((lower_bound(v.begin(),v.end(),s.top())));
+				s.pop();
+			}
+		}
+		else if(name=="Push")
+		{
+			cin>>m;
+			s.push(m);
+//			vector<int>::iterator j=lower_bound(v.begin(),v.end(),t);
+//			v.insert(j,t);			
+			v.insert(lower_bound(v.begin(),v.end(),m),m);
+		}
+		else if(name=="PeekMedian")
+		{
+			if(s.empty())
+			{
+				cout<<"Invalid"<<endl;
+			}
+			
+			else
+			{
+//				cout<<v[(v.size()+1)/2-1]<<endl;
+				cout<<v.at((v.size()+1)/2-1)<<endl;
+			}
+			
+		}
+		else
+		{
+			;
+			
+			
+		}
+
+		
+	}
+	
+
+	
+	return 0;
+ }
